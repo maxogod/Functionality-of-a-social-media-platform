2f419baa2430a47d2fb8b077ac224b6f99e0aaf0
santi <santisevitz@hotmail.com>
2022-11-12 15:23:41 -0300

    main package

--- algogram.go
+++ algogram.go
@@ -1,1 +1,31 @@
 package main
+
+import (
+	"algogram/errores"
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+var ARCHIVOS = os.Args[1:]
+
+func main() {
+	// TODO procesar archivos
+	scan := bufio.NewScanner(os.Stdin)
+	for scan.Scan() {
+		entrada := strings.Split(scan.Text(), " ")
+		comando := entrada[0]
+
+		switch comando {
+		case "login":
+		case "logout":
+		case "publicar":
+		case "ver_siguiente_feed":
+		case "likear_post":
+		case "mostrar_likes":
+		default:
+			fmt.Println(new(errores.ErrorParametros))
+		}
+	}
+}
