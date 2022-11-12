fcaf06c49c02cb89a324406de4b95a1b714289c5
santi <santisevitz@hotmail.com>
2022-11-12 18:58:28 -0300

    Implementacion de TDA usuario

--- TDAs_algogram/usuario/usuario.go
+++ TDAs_algogram/usuario/usuario.go
@@ -1,4 +1,13 @@
 package usuario
 
+import "algogram/TDAs_algogram/post"
+
 type Usuario interface {
+
+	// VerSigPost devuelve el siguiente post disponible para ver de un usuario basado en la afinidad que este tenga
+	// con el poster
+	VerSigPost() (string, error)
+
+	// AgregarPostFeed agrega el post pasado por parametro a la feed de el usuario
+	AgregarPostFeed(post post.Post)
 }
--- TDAs_algogram/usuario/usuario_implementacion.go
+++ TDAs_algogram/usuario/usuario_implementacion.go
@@ -1,4 +1,46 @@
 package usuario
 
+import (
+	hp "algogram/TDAs/heap"
+	post "algogram/TDAs_algogram/post"
+	"algogram/errores"
+	"strings"
+)
+
 type usuario struct {
+	feed   hp.ColaPrioridad[post.Post]
+	nombre string
+}
+
+// CrearUsuario Funcion de creacion de un usuario
+func CrearUsuario(nombre string) Usuario {
+	usuarioCreado := new(usuario)
+	usuarioCreado.feed = hp.CrearHeap[post.Post](cmp)
+	usuarioCreado.nombre = nombre
+	return usuarioCreado
+}
+
+// Metodos de usuario abajo
+
+func (u *usuario) VerSigPost() (string, error) {
+	if u.feed.EstaVacia() {
+		return "", errores.ErrorVerPost{}
+	}
+	return u.feed.Desencolar().MostrarPost(), nil
+}
+
+func (u *usuario) AgregarPostFeed(post post.Post) {
+	u.feed.Encolar(post)
+}
+
+// Funciones adicionales abajo
+
+// cmp Funcion de comparacion basada en afinidad de usuarios
+func cmp(post1, post2 post.Post) int {
+	if strings.Compare(post1.ObtenerPoster().nombre, post2.ObtenerPoster().nombre) > 1 {
+		return 1
+	} else if strings.Compare(post1.ObtenerPoster().nombre, post2.ObtenerPoster().nombre) < 1 {
+		return -1
+	}
+	return 0
 }
