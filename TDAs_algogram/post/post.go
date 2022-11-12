1a4363657738945655e21a789151d6d3ca9c1c4e
santisev <santisevitz@hotmail.com>
2022-11-12 14:26:12 -0800

    implementacion POST interface

--- TDAs_algogram/post/post.go
+++ TDAs_algogram/post/post.go
@@ -1,4 +1,13 @@
 package post
 
+import "algogram/TDAs_algogram/usuario"
+
 type Post interface {
+	//MostrarLikes muestra la cantidad de likes y quienes likearon el post en question
+	MostrarLikes() string
+	//MostrarPost muestra el post actual
+	MostrarPost() string
+	//Devolvemos el dueño del post
+	DevolverPosteador() usuario.Usuario
+	LikearPost(usuarioNombre string) string
 }
--- TDAs_algogram/post/post_implementacion.go
+++ TDAs_algogram/post/post_implementacion.go
@@ -1,4 +1,48 @@
 package post
 
+import (
+	TDAdic "algogram/TDAs/abb"
+	"algogram/TDAs_algogram/usuario"
+	"fmt"
+	"strings"
+)
+
 type post struct {
+	descripcion      string
+	likes            TDAdic.DiccionarioOrdenado[string, string]
+	usuarioPosteador usuario.Usuario
+	id               int
+}
+
+func (p post) LikearPost(usuarioNombre string) string {
+	if !p.likes.Pertenece(usuarioNombre) {
+		p.likes.Guardar(usuarioNombre, usuarioNombre)
+	}
+	return "Post likeado"
+}
+
+func (p post) DevolverPosteador() usuario.Usuario {
+	return p.usuarioPosteador
+}
+
+func (p post) MostrarLikes() string {
+	var nombre string
+	p.likes.Iterar(func(clave string, dato string) bool {
+		nombre += fmt.Sprintf("\n%s", dato)
+		return true
+	})
+	return fmt.Sprintf("El post tiene %d likes:%s", p.likes.Cantidad())
+}
+
+func (p post) MostrarPost() string {
+	return fmt.Sprintf("Post ID %d\n%s\nLikes: %d", p.id, p.descripcion, p.likes.Cantidad())
+}
+
+func CrearPost(id int, descripcion string, usuarioPosteador usuario.Usuario) Post {
+	p := new(post)
+	p.descripcion = descripcion
+	p.likes = TDAdic.CrearABB[string, string](strings.Compare)
+	p.usuarioPosteador = usuarioPosteador
+	p.id = id
+	return p
 }
