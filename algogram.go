853c73453e622ea498498b8cb3daaf9325d78efd
maxogod <maxi18wars@gmail.com>
2022-11-13 12:46:21 -0300

    Login - Logout - VersiguienteFeed

--- algogram.go
+++ algogram.go
@@ -16,12 +16,13 @@ var ARGS = os.Args[1:]
 
 func main() {
 	var (
-		usuarios = diccionario.CrearHash[string, usuario.Usuario]()
-		posts    = diccionario.CrearHash[int, post.Post]()
+		usuarios                 = diccionario.CrearHash[string, usuario.Usuario]()
+		posts                    = diccionario.CrearHash[int, post.Post]()
+		logueado usuario.Usuario = nil
 	)
-	err := procesar_archivos.ObtenerTodosUsuarios(ARGS, usuarios)
-	if err != nil {
-		print(err.Error())
+	errDeProcesamiento := procesar_archivos.ObtenerTodosUsuarios(ARGS, usuarios)
+	if errDeProcesamiento != nil {
+		fmt.Println(errDeProcesamiento)
 		return
 	}
 	scan := bufio.NewScanner(os.Stdin)
@@ -31,9 +32,38 @@ func main() {
 
 		switch comando {
 		case "login":
+			nombreUsuario := entrada[1]
+			if usuarios.Pertenece(nombreUsuario) && logueado == nil {
+				logueado = usuarios.Obtener(nombreUsuario)
+				fmt.Println(fmt.Sprintf("Hola %s", logueado.ObtenerNombre()))
+			} else if logueado != nil {
+				fmt.Println(errores.ErrorLogin{})
+			} else {
+				fmt.Println(errores.ErrorUsuarioInvalido{})
+			}
+
 		case "logout":
+			if logueado != nil {
+				logueado = nil
+				fmt.Println("Adios")
+			} else {
+				fmt.Println(errores.ErrorSinUsuarioLogueado{})
+			}
+
 		case "publicar":
+
 		case "ver_siguiente_feed":
+			if logueado != nil {
+				siguientePost, err := logueado.VerSigPost()
+				if err == nil {
+					fmt.Println(siguientePost)
+				} else {
+					fmt.Println(err)
+				}
+			} else {
+				fmt.Println(errores.ErrorVerPost{})
+			}
+
 		case "likear_post":
 		case "mostrar_likes":
 		default:
