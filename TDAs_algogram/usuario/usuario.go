2317fe710fb61a2e46ea0372d3a7020bb1d3ebb2
maxogod <maxi18wars@gmail.com>
2022-11-13 12:45:38 -0300

    actualizacion de documentacion

--- TDAs_algogram/usuario/usuario.go
+++ TDAs_algogram/usuario/usuario.go
@@ -5,7 +5,7 @@ import "algogram/TDAs_algogram/post"
 type Usuario interface {
 
 	// VerSigPost devuelve el siguiente post disponible para ver de un usuario basado en la afinidad que este tenga
-	// con el poster
+	// con el poster, devuelve error si no hay nada en feed
 	VerSigPost() (string, error)
 
 	// AgregarPostFeed agrega el post pasado por parametro a la feed de el usuario
